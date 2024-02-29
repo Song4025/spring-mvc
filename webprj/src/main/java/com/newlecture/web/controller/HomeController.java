22de2470a67f7d7af8f796cf7b3ae5f15168fe20
thdgus4025 <thdgus4025@gmail.com>
2024-02-29 17:57:01 +0900

    form 수정

--- webprj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,15 +1,11 @@
 package com.newlecture.web.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Component
 @RequestMapping("/")
 public class HomeController {
 
--- webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,34 +1,76 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice/")
 public class NoticeController { // <bean name="noticeController" class="com.newlecture.web.controller.admin.board" />
 
+	@Autowired
+	private ServletContext ctx;
+	
 	@RequestMapping("list")
 	public String list() {
-		return "";
+		return "admin.board.notice.list";
 	}
 	
-	@RequestMapping("reg")
-	@ResponseBody
-	public String reg(String title, String content, String category, String[] foods, String food) {
+	@GetMapping("reg")
+	public String reg() {
+		return "admin.board.notice.reg";
+	}
+	
+	@PostMapping("reg")
+	public String reg(HttpServletRequest request, String title, String content, MultipartFile[] files, String category, String[] foods, String food) throws IllegalStateException, IOException {
+		
+		for(MultipartFile file : files) {
+		
+			String fileName = file.getOriginalFilename();
+			long size = file.getSize();
+			System.out.printf("fileName: %s, fileSize: %d\n", fileName, size);
+			
+			// ServletContext ctx = request.getServletContext();
+			
+			String webPath = "/static/upload";
+			String realPath = ctx.getRealPath(webPath);
+			System.out.printf("realPath: %s\n", realPath);
+			File savePath = new File(realPath);
+			// 경로가 존재하지않으면 경로만들어주기
+			if(!savePath.exists())
+				savePath.mkdirs();
+			
+			realPath += File.separator + fileName; // 
+			File saveFile = new File(realPath);
+			
+			file.transferTo(saveFile);
+		}
+		
 		for(String food1 : foods)
 			System.out.println(food1);
-		return String.format("title: %s<br> content: %s<br> category: %s<br> favorite food: %s", title, content, category, food);
+		return "admin.board.notice.reg";
+		
+		//return String.format("title: %s<br> content: %s<br> category: %s<br> favorite food: %s", title, content, category, food);
 	}
 	
 	@RequestMapping("edit")
 	public String edit() {
-		return "";
+		return "admin.board.notice.edit";
 	}
 	
 	@RequestMapping("del")
 	public String del() {
-		return "";
+		return "admin.board.notice.list";
 	}
 	
 }
